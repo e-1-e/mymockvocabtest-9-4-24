f6ccfebffa0c769c5a00febe61d97e5090535984
e-1-e <155574206+e-1-e@users.noreply.github.com>
2024-11-15 19:42:25 -0500

    swag

--- TKAM1/scripts.js
+++ TKAM1/scripts.js
@@ -41,7 +41,6 @@ while (count < Object.keys(wordList).length) {
     var newEle = curCol.querySelector("#demo").cloneNode(true);
     newEle.id = '';
     newEle.removeAttribute('hidden');
-    newEle.querySelector('input').disabled = true;
 
     newEle.querySelector('p').innerText = e;
 
@@ -51,64 +50,3 @@ while (count < Object.keys(wordList).length) {
     
 }
 
-//timers and triggers
-var simpleTimer = 999999999;
-var c_timer;
-
-document.body.querySelector("#b_start").addEventListener('click', function(){
-    wordEntries.forEach((e)=>{e.querySelector('input').disabled = false;})
-
-    document.body.removeChild(document.body.querySelector("#b_start"));
-});
-
-document.body.querySelector("#b_submit").addEventListener("click", function(){
-    grade();
-    return
-})
-
-
-//grading system skibidi
-/*
-if one does decide to update this to grade the last 5 qs using ai...
-chatgpt : tell me the roots in '[word]' without explanation. just the roots.
-
-ACTUALLY MERRIAM WEBSTER DICTIONARY API FREE WOOOOOOOOOOOOOOOOOOOO
-only problem is api key securing and api key limit but yuh
-*/
-function grade(){
-    let score = 1 - 1;
-    wordEntries.forEach((e)=>{
-        e.querySelector('input').setAttribute('disabled', 'true');
-        e.querySelector('input').value = e.querySelector('input').value.trim();
-
-        if (typeof wordList[e.querySelector('p').innerText] == 'object') {
-            let correct = false;
-            for (i in wordList[e.querySelector('p').innerText]) {
-                if (e.querySelector('input').value.toLowerCase() == wordList[e.querySelector('p').innerText][i]) {
-                    correct = true;
-                }
-            }
-            if (correct) {
-                score++;
-            } else {
-                e.querySelector('input').style.backgroundColor = 'pink';
-                e.querySelector('input').style.color = 'green';
-                e.querySelector('input').style.fontWeight = '900';
-                e.querySelector('input').value = wordList[e.querySelector('p').innerText][0];
-            }
-            return
-        }
-
-        if (e.querySelector('input').value.toLowerCase() == wordList[e.querySelector('p').innerText]) {
-            score++;
-        } else {
-            e.querySelector('input').style.backgroundColor = 'pink';
-            e.querySelector('input').style.color = 'green';
-            e.querySelector('input').style.fontWeight = '900';
-            e.querySelector('input').value = wordList[e.querySelector('p').innerText];
-        }
-    });
-    document.body.querySelector("#t_timer").innerText = `${score}/10`;
-    document.body.querySelector("#b_submit").setAttribute("disabled", "true");
-    document.body.querySelector("#b_submit").innerText = 'REFRESH TO RETAKE';
-}
