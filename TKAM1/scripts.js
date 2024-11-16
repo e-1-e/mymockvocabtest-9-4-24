9a157a83da374c2bbc3009626b750babbbf89ea2
e-1-e <155574206+e-1-e@users.noreply.github.com>
2024-11-15 19:51:08 -0500

    try

--- TKAM1/scripts.js
+++ TKAM1/scripts.js
@@ -7,8 +7,8 @@ multiple choice exam since free response won't work
 */
 
 var wordList = {
-    'assuage' : ['make (an unpleasant feeling) less intense)', 'make less intense'],
-    'seldom' : ['not often\; rarely', 'not often', 'rarely'],
+    'assuage' : 'make (an unpleasant feeling) less intense)',
+    'seldom' : 'not often\; rarely',
     'enable' : 'to give someone or something the authority or means to do something',
     'revelation' : 'a surprising and previously unknown fact, especially one that is made known in a dramatic way',
     'habitually' : 'by way of habit; customarily',
@@ -24,27 +24,41 @@ var wordEntries = [];
 //set up HTML
 let count = 0;
 let positionsTaken = [];
+let positionsTaken2 = [];
 
 while (count < Object.keys(wordList).length) {
     let newPosition = Math.ceil(Math.random() * Object.keys(wordList).length - 1);
+    let newPosition2 = Math.ceil(Math.random() * Object.keys(wordList).length - 1);
     
     while (positionsTaken.indexOf(newPosition) != -1) {
         newPosition = Math.ceil(Math.random() * Object.keys(wordList).length) - 1;
     }
+    while (positionsTaken2.indexOf(newPosition2) != -1) {
+        newPosition2 = Math.ceil(Math.random() * Object.keys(wordList).length) - 1;
+    }
+
     console.log(newPosition);
     positionsTaken.push(newPosition);
 
     let e = Object.keys(wordList)[newPosition]
+    let f = Object.values(wordList)[newPosition2];
 
-    var curCol = document.getElementById((count < 13) ? 'm_col1' : 'm_col2');
-    console.log(curCol);
-    var newEle = curCol.querySelector("#demo").cloneNode(true);
+    var newEle = document.getElementById('m_col1').querySelector("#demo").cloneNode(true);
     newEle.id = '';
     newEle.removeAttribute('hidden');
 
     newEle.querySelector('button').innerText = e;
 
-    curCol.append(newEle);
+    document.getElementById('m_col1').append(newEle);
+
+    var newEle2 = document.getElementById('m_col2').querySelector("#demo").cloneNode(true);
+    newEle2.id = '';
+    newEle2.removeAttribute('hidden');
+
+    newEle2.querySelector('button').innerText = f;
+
+    document.getElementById('m_col1').append(newEle2);
+
     wordEntries.push(newEle);
     count++;
     
